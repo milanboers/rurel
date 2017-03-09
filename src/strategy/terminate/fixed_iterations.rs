dc2aa4592ac07d3a484800d8acbffbf0c7c4ae46
Milan Boers <mail@milanboers.nl>
2017-03-09 18:36:22 +0100

    Added termination strategies

---
+++ src/strategy/terminate/fixed_iterations.rs
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! Module for the fixed iterations strategy.
+
+use mdp::State;
+use strategy::terminate::TerminationStrategy;
+
+/// The termination strategy that ends after a certain number of iterations, regardless of the
+/// `State`.
+pub struct FixedIterations {
+    i: u32,
+    iters: u32,
+}
+
+impl FixedIterations {
+    /// Constructs a new termination strategy which ends when `iters` value updates have occurred.
+    pub fn new(iters: u32) -> FixedIterations {
+        FixedIterations {
+            i: 0,
+            iters: iters,
+        }
+    }
+}
+
+impl<S: State> TerminationStrategy<S> for FixedIterations {
+    fn should_stop(&mut self, _: &S) -> bool {
+        self.i += 1;
+        self.i > self.iters
+    }
+}
---
+++ src/strategy/terminate/mod.rs
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! Module containing termination strategies.
+
+pub mod fixed_iterations;
+
+pub use self::fixed_iterations::FixedIterations;
+
+use mdp::State;
+
+/// A termination strategy decides when to end training.
+pub trait TerminationStrategy<S: State> {
+    /// If `should_stop` returns `true`, training will end.
+    fn should_stop(&mut self, state: &S) -> bool;
+}
