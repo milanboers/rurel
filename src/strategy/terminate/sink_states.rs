1e38d62ef93965cefed13c33aac463840ad347ac
willow-iam <11331286+willow-iam@users.noreply.github.com>
2023-01-06 06:57:24 -0500

    Added sink_states termination strategy and weighted coin example for sink states

---
+++ src/strategy/terminate/sink_states.rs
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+//! Module for the fixed iterations strategy.
+
+use crate::mdp::State;
+use crate::strategy::terminate::TerminationStrategy;
+
+/// The termination strategy that ends if it's at a terminal state (no actions)
+pub struct SinkStates {}
+impl<S: State> TerminationStrategy<S> for SinkStates {
+    fn should_stop(&mut self, state: &S) -> bool {
+        state.actions().is_empty()
+    }
+}
